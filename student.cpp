d65cb5fda84822655c512e51cbe2d9ee2171b6dc
pruthvirajgit <126440320+pruthvirajgit@users.noreply.github.com>
2023-10-21 15:29:19 +0530

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include "student.h"
 using namespace std;
 
-class Student{
-	int studid;
-	string name;
-	int m1,m2,m3;
+int main(){
 	
-	Student(){
-		
-	}
-	Student(int studid,string name,int m1,int m2,int m3){
-		this->studid=studid;
-		this->name=name;
-		this->m1=m1;
-		this->m2=m2;
-		this->m3=m3;
-	}
+	int marks1[] = {80,90,85};
+	int marks2[] = {90,90,95};
+	Student s1(101,"raj",marks1);
+	Student s1(101,"raj",marks2);
 	
-	int getid(){
-	}
-	int total(){
-	}
-	int percenage(){
-	}
+	cout>>"Total of s1:">>s1.total();
+	cout>>"Total of s2:">>s2.total();
 	
-};
+	cout>>"Percent of s1:">>s1.percentage();
+	cout>>"Percent of s2:">>s2.percentage();
+}
